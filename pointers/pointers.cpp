9a730433e51f4c9c17b97d0b971bf8a251df71bc
Sammi <songyunhui2008@gmail.com>
2020-08-18 20:16:53 -0700

    change pointer value.

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -1,36 +1,31 @@
-/*
-    Write a program in C to show the basic declaration of pointer
-
-    Expected Output :
-
-    Pointer: Show the basic declaration of pointer :
-    ------------------------------------------------------ -
-    Here is m = 10, n and o are two integer variable and *z is an integer
-
-    z stores the address of m = 0x7ffd40630d44
-
-    * z stores the value of m = 10
-
-    & m is the address of m = 0x7ffd40630d44
-
-    & n stores the address of n = 0x7ffd40630d48
-
-    & o  stores the address of o = 0x7ffd40630d4c
-
-    & z stores the address of z = 0x7ffd40630d50
-*/
-
-
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
-    int m = 10;
-    int n, o;
-    int* z;
-    z = &m;
+	//change pointer value
+	int m = 29;
 
-    std::cout << "z = " << z << std::endl << "*z =" << *z << std::endl <<"&m =" << &m << std::endl <<"&n="<< &n << std::endl << "&o=" <<&o << std::endl <<"&z=" << &z << std::endl;
+	cout << "&m = " << &m << "\tm=" << m << endl;
+
+	int* ab = &m;
+
+	cout << "&m = " << &m << "\tm=" << m << endl;
+
+	*ab = 34;
+	
+	cout << "&m = " << &m << "\tm=" << m << endl;
+
+	*ab = 7;
+
+	cout << "&m = " << &m << "\tm=" << m << endl;
+
+	// &m = 0x???
+	// ab = &m
+	// m  = 29
+	// *ab = 7
+	cout << "&m = " << &m << "\tab=" << ab << "\tm=" << m << "\t*ab=" << *ab << endl;
+
 
 }
